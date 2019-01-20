1ef39c00ee112e9b46e66e0703abcf8a722b6640
lgh7418 <lgh7418@gmial.com>
2019-01-20 16:41:16 +0900

    update

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,5 +1,16 @@
 import express from "express";
+import routes from "../routes";
+import { home, search } from "../controller/videoController";
+import { join, login, logout } from "../controller/userController";
 
 const globalRouter = express.Router();
+
+// 함수를 controller에 모아서 예쁘게 정리하고, export해서 여기다가 import 하는 거임
+// globalRouter.get(routes.home, (req, res) => res.send('Home'));
+globalRouter.get(routes.home, home);
+globalRouter.get(routes.search, search);
+globalRouter.get(routes.join, join);
+globalRouter.get(routes.login, login);
+globalRouter.get(routes.logout, logout);
 
 export default globalRouter;
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,5 +1,13 @@
 import express from "express";
+import routes from "../routes";
+import { videos, upload, videoDetail, editVideo, deleteVieo } from "../controller/videoController";
 
 const videoRouter = express.Router();
+
+videoRouter.get(routes.videos, videos);
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.deleteVideo, deleteVieo);
 
 export default videoRouter;
